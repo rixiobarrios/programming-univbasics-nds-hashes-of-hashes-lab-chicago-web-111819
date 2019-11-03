1598f0b0086cdef9b2172dd2a1c1c860f0074f5c
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-11-03 20:51:27 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,5 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
- Hash.new
+ PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
 end
