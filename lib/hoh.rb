f0ea759124f3c041111c75676a814e9a76c43335
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-11-03 20:57:03 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -42,5 +42,5 @@ def naming_system
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
  PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
- p PORTION_1
+ PORTION_1
 end
